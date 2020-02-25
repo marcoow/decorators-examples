007aba5b0cf0884e2463dc18deab30cca2642dad
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-02-25 12:10:32 +0100

    require authentication for data route

--- app/routes/data.js
+++ app/routes/data.js
@@ -1,7 +1,17 @@
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
 import benchmark from '../decorators/benchmark';
 
 export default class DataRoute extends Route {
+  @inject
+  session;
+
+  beforeModel() {
+    if (!this.session.loggedIn) {
+      this.transitionTo('index');
+    }
+  }
+
   @benchmark('/data – model')
   async model() {
     return new Promise(function(resolve) {
